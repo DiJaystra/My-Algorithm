ba9c2d119d58f77db911ed874e2df24b1400dd54
DiJaystra <dijaystra@gmail.com>
2026-06-28 21:05:31 +0800

    迪杰斯特拉板子——修改变量名、全局数组

--- GraphTheory/dijkstra.cpp
+++ GraphTheory/dijkstra.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
-using ull=unsigned long long;
-using ld=long double;
-using lll=__int128;
 
-int n,m;
-vector<vector<pair<int,ll>>> graph;
-vector<ll> dist; //单源最短路径长度
+int MAXN=2e5+5;
+int n,m,s; //点数，边数，起点
+vector<vector<pair<int,ll>>> graph(MAXN);
+vector<ll> dist(MAXN,LLONG_MAX); //单源最短路径长度
 
 void dijkstra(int s) {
     dist[s]=0;
-    priority_queue<pair<ll,int>,vector<pair<ll,int>>,greater<>> pq;
+    priority_queue<pair<ll,int>,vector<pair<ll,int>>,greater<pair<ll,int>>> pq;
     pq.push({0,s});
     while(!pq.empty()) {
         auto [now,u]=pq.top();pq.pop();
         if(now > dist[u]) continue; //关键优化
-        for(auto [v,val]:graph[u]) {
-            ll tot=now+val;
+        for(auto [v,w]:graph[u]) {
+            ll tot=now+w;
             if(tot<dist[v]) {
                 dist[v]=tot;
                 pq.push({tot,v});
@@ -29,10 +27,7 @@ void dijkstra(int s) {
 signed main() {
     ios::sync_with_stdio(false);cin.tie(0);
     
-    int s; //起点
     cin>>n>>m>>s;
-    graph.resize(n+1);
-    dist.resize(n+1,LLONG_MAX);
     for(int i=0;i<m;i++) {
         int u,v,w;cin>>u>>v>>w;
         graph[u].push_back({v,w}); //注意这里是有向图
