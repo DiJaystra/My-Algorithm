7a8d2d69eedb4cb6a9380b56fc726a3a62fe3fb8
DiJaystra <dijaystra@gmail.com>
2026-08-11 23:48:24 +0800

    调整迪杰斯特拉和弗洛伊德算法模板

--- GraphTheory/Floyd.cpp
+++ GraphTheory/Floyd.cpp
@@ -1,14 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
-using ull=unsigned long long;
-using ld=long double;
-using lll=__int128;
 
-void floyd(vector<vector<ll>> &dist,int n) {
-    for(int k=0;k<n;k++) {
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
+const int MAXN=205; //最大点数
+
+int n,m; //点数，边数
+ll dist[MAXN][MAXN]; //最短距离数组
+
+void floyd() {
+    for(int k=1;k<=n;k++) { //枚举中转点
+        for(int i=1;i<=n;i++) { //枚举起点
+            for(int j=1;j<=n;j++) { //枚举终点
                 if(dist[i][k] != LLONG_MAX && dist[k][j] != LLONG_MAX) {
                     dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
                 }
@@ -19,12 +21,11 @@ void floyd(vector<vector<ll>> &dist,int n) {
 
 signed main() {
     ios::sync_with_stdio(false);cin.tie(0);
-
-    int n,m;cin>>n>>m;
-    vector<vector<ll>> dist(n+1,vector<ll> (n+1,LLONG_MAX)); //最短距离数组
+    cin>>n>>m;
+    fill(&dist[0][0],&dist[0][0]+MAXN*MAXN,LLONG_MAX);
     for(int i=1;i<=n;i++) dist[i][i]=0;
 
-    for(int i=0;i<m;i++) {
+    for(int i=1;i<=m;i++) {
         int u,v;cin>>u>>v;
         ll w;cin>>w;
         //支持重边与自环
@@ -32,7 +33,6 @@ signed main() {
         dist[v][u]=min(dist[v][u],w);
     }
 
-    floyd(dist,n+1); //1-index 传n+1
-
+    floyd(); //1-index 传n+1
     return 0;
 }
--- GraphTheory/dijkstra.cpp
+++ GraphTheory/dijkstra.cpp
@@ -2,21 +2,22 @@
 using namespace std;
 using ll=long long;
 
-int MAXN=2e5+5;
-int n,m,s; //点数，边数，起点
-vector<vector<pair<int,ll>>> graph(MAXN);
-vector<ll> dist(MAXN,LLONG_MAX); //单源最短路径长度
+const int MAXN=2e5+5; //最大点数
 
-void dijkstra(int s) {
-    dist[s]=0;
+int n,m,s; //点数，边数，起点
+vector<vector<pair<int,ll>>> graph; //邻接表
+ll dist[MAXN]; //单源最短路径长度
+
+void dijkstra() {
     priority_queue<pair<ll,int>,vector<pair<ll,int>>,greater<pair<ll,int>>> pq;
+    dist[s]=0;
     pq.push({0,s});
     while(!pq.empty()) {
         auto [now,u]=pq.top();pq.pop();
         if(now > dist[u]) continue; //关键优化
         for(auto [v,w]:graph[u]) {
             ll tot=now+w;
-            if(tot<dist[v]) {
+            if(tot < dist[v]) {
                 dist[v]=tot;
                 pq.push({tot,v});
             }
@@ -26,18 +27,18 @@ void dijkstra(int s) {
 
 signed main() {
     ios::sync_with_stdio(false);cin.tie(0);
-    
     cin>>n>>m>>s;
-    for(int i=0;i<m;i++) {
+    graph.resize(MAXN);
+    fill(dist,dist+MAXN,LLONG_MAX);
+    for(int i=1;i<=m;i++) {
         int u,v,w;cin>>u>>v>>w;
         graph[u].push_back({v,w}); //注意这里是有向图
     }
 
-    dijkstra(s);
+    dijkstra();
     for(int i=1;i<=n;i++) {
         cout<<dist[i]<<' ';
     }
     cout<<'\n';
-    
     return 0;
 }
