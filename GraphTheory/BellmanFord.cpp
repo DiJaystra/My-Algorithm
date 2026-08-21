091b461791be30788110306842378c76afb30a66
DiJaystra <dijaystra@gmail.com>
2026-08-21 12:48:42 +0800

    添加贝尔曼福特算法模板，同时SPFA算法添加一些详细注释

--- GraphTheory/BellmanFord.cpp
+++ GraphTheory/BellmanFord.cpp
@@ -3,6 +3,7 @@ using namespace std;
 using ll=long long;
 
 const int MAXN=2e5+5;
+const int MAXM=2e5+5;
 
 int n,m; //点数、边数
 //邻接表
@@ -10,42 +11,41 @@ vector<vector<pair<int,ll>>> graph;
 //距离
 ll dist[MAXN];
 
-//节点是否在队列中
-bool inque[MAXN];
-//节点被几次加入队列
-int times[MAXN];
+//边结构体
+struct edge {
+    int u,v;
+    ll w;
+};
+edge E[MAXM]; //把所有边记下来
 
 //起点为s，跑最短路算法，返回是否有负环
 //注意，只能判断从s出发是否能到达负环
 //若需判断整张图有无负环，要用虚拟源点作为出发点
 //虚拟源点连接原图中所有实际点
-//复杂度O(nm)，只是常数时间有优化
-bool spfa(int s) {
+//复杂度O(nm)
+bool bellman(int s) {
     dist[s]=0;
-    //将起点入队
-    queue<int> que;
-    que.push(s);
-    inque[s]=true;
-    times[s]=1;
-
-    while(!que.empty()) {
-        int u=que.front();que.pop();
-        inque[u]=false;
-        for(auto [v,w] : graph[u]) {
-            if(dist[u]+w < dist[v]) {
+    int cnt=0; //松弛轮数
+    while(1) {
+        //是否有更短距离的更新
+        bool update=false;
+        //每轮遍历每条边
+        for(int i=1;i<=m;i++) {
+            auto [u,v,w]=E[i];
+            //到v距离更短
+            if(dist[u]!=LLONG_MAX&&dist[u]+w<dist[v]) {
                 dist[v]=dist[u]+w;
-                if(!inque[v]) {
-                    que.push(v);
-                    inque[v]=true;
-                    times[v]++;
-                    //加入队列次数大于等于n则松弛过多，有负环
-                    if(times[v]>=n) {
-                        return true;
-                    }
-                }
+                update=true;
             }
         }
+        //没有松弛操作，则所有最短路都已经找到
+        if(!update) break;
+        //否则松弛次数+1
+        cnt++;
+        //最多松弛n-1次，超过则有负环
+        if(cnt>=n) return true;
     }
+    //无负环
     return false;
 }
 
@@ -58,10 +58,11 @@ signed main() {
         int u,v;ll w;
         cin>>u>>v>>w;
         graph[u].push_back({v,w}); //有向图
+        E[i]={u,v,w};
     }
 
     int s;cin>>s;
-    bool neg=spfa(s);
+    bool neg=bellman(s);
     if(neg) {
         cout<<"-1\n";
         return 0;
